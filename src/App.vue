a8d93dd26ff09a2f4b1dc627bdfd3361eb779610
Silin Na <n1207n@gmail.com>
2017-01-05 21:50:22 -0800

    feat: Deleted Hello component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
 
 export default {
   name: 'app',
   components: {
-    Hello,
   },
 };
 </script>
