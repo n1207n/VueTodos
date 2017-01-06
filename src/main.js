657f4aad0f89cb313bd9c9a78af34a5d3c28d465
Silin Na <n1207n@gmail.com>
2017-01-05 21:55:47 -0800

    feat: Added some Vue libraries and enabled them

--- src/main.js
+++ src/main.js
@@ -1,7 +1,29 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.css';
+import VueForm from 'vue-form';
+import VueResource from 'vue-resource';
+
 import App from './App';
+
+Vue.use(VueMaterial);
+Vue.use(VueForm);
+Vue.use(VueResource);
+
+Vue.material.registerTheme({
+  default: {
+    primary: 'blue',
+    accent: 'red',
+  },
+
+  teal: {
+    primary: 'teal',
+    accent: 'pink',
+  },
+});
+Vue.material.setCurrentTheme('teal');
 
 /* eslint-disable no-new */
 new Vue({
